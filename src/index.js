ef7058d414da5279b6bb38023c0ebb38633d07bf
Alexei Ivkin <ivkin.alexey@yandex.ru>
2020-03-18 20:42:11 +0300

    feat: create towelSort algorithm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    if (!matrix) return [];
+    return matrix.reduce((flat, current, i) => i % 2 !== 0 ? flat.concat(current.reverse()) : flat.concat(current), []);
+};
